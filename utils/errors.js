ddff9501edd664ee66c704b6857c222f9c5c08c3
Lukylix <lucas.garcia.bobi@gmail.com>
2020-07-31 13:15:41 +0200

    :alembic: errorsHandlers error.js

---
+++ utils/errors.js
@@ -0,0 +1,12 @@
+// args contain req, res, next
+const errorHandler = (ourFunction) => {
+	return (req, res, next) => {
+		return ourFunction(req, res, next).catch(next);
+	};
+};
+
+// prevent node from crashing if async call failed
+const unhandledRejection = (reason, promise) => {
+	console.log("Unhandled Rejection at:", promise, "reason:", reason);
+	// TODO Store those errors somewhere
+};
