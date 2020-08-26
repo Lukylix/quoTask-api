f888680b4c4b39d73892342f62e3de6d310af8e5
Lukylix <lucas.garcia.bobi@gmail.com>
2020-08-26 17:19:08 +0200

    :art: Group routes inside app.js

--- app.js
+++ app.js
@@ -6,37 +6,24 @@ require("./configs/mongoose");
 
 //BodyParser is include inside express by default
 app.use(express.json());
-
-// Validate Json
 app.use((err, req, res, next) => {
 	if (err) res.status(400).send({ message: "Json parsing error" });
-	// Next is called by default if present on args
+	next()
 });
 
-app.get("/", (req, res) => {
-	//Send a response with 302 status (Found)
-	res.status(302).json("Welcome to our api");
-});
-
+app.get("/", (req, res) => res.status(302).json("Welcome to our api"));
 app.use("/login", require("./routes/login"));
-
-//Middleware for routes that start with user or users
 app.use("/users?", require("./routes/users"));
 
-// Middleware auth validation
-app.use(require("./controllers/login").verify);
-
+app.use(require("./controllers/login").verify); // Middleware auth validation
 app.use("/workspaces?", require("./routes/workspaces"));
-
 app.use("/childs?", require("./routes/childs"));
 
-app.use((req, res) => {
-	res.status(404).json({ message: "Page not found" });
-});
+app.use((req, res) => res.status(404).json({ message: "Page not found" }));
 
 // Catching errors
-// err.status never used
 app.use((err, req, res, next) => {
+	// err.status never used
 	res.status(err.status || 500).json({
 		message: err.message || "Internal Server Error",
 		err,
