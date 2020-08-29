529452f59303e3528e734bd38093009ac5ee7936
Lukylix <lucas.garcia.bobi@gmail.com>
2020-08-29 03:36:36 +0200

    :art: Cleaning Childs controller
    how could i previously see throught this mess ???

--- controllers/childs.js
+++ controllers/childs.js
@@ -1,17 +1,16 @@
 const Workspace = require("../models/Workspace").model;
 const mongoose = require("mongoose");
+
 // TODO Facultative precise get
 
 exports.createChild = (req, res) => {
-	const { childPath, arrayFilters, target_id } = preparePath(req, res);
-	// if prepare fail we doesn't wont to run a bad querry
-	if (!childPath) return;
+	const { childPath, arrayFilters } = prepareQueryOptions(req, res);
+	if (!childPath) return; // if prepare fail we wont run a bad query
 
-	delete req.body.path;
 	return Workspace.updateOne(
 		{ _id: req.auth.workspace },
 		{
-			$push: { [childPath]: { ...req.body, _id: target_id } },
+			$push: { [childPath]: req.body },
 		},
 		{
 			arrayFilters,
@@ -33,9 +32,9 @@ exports.createChild = (req, res) => {
 };
 
 exports.updateChild = (req, res) => {
-	const { $setArray, arrayFilters } = preparePath(req, res);
-	// if prepare fail we doesn't wont to run a bad querry
-	if (!$setArray) return;
+	const { $setArray, arrayFilters } = prepareQueryOptions(req, res);
+	if (!$setArray) return; // if prepare fail we doesn't wont to run a bad query
+
 	return Workspace.updateOne(
 		{ _id: req.auth.workspace },
 		{
@@ -52,7 +51,7 @@ exports.updateChild = (req, res) => {
 				result,
 			});
 
-		// Accepted
+		// Accepted status
 		res.status(200).json({
 			message: "Nothing to update",
 			result,
@@ -61,8 +60,9 @@ exports.updateChild = (req, res) => {
 };
 
 exports.deleteChild = (req, res) => {
-	const { childPath, arrayFilters, target_id } = preparePath(req, res);
+	const { childPath, arrayFilters, target_id } = prepareQueryOptions(req, res);
 	if (!childPath) return;
+
 	return Workspace.updateOne(
 		{ _id: req.auth.workspace },
 		{
@@ -88,35 +88,79 @@ exports.deleteChild = (req, res) => {
 	});
 };
 
-function preparePath(req, res) {
+const dicPathExemples = {
+	default: {
+		path: "categories[].tasks[5f16a6adc048c15d3d1889df]",
+		alternative: "categories[5f16a5e17c284c5c6d9a6f95].tasks[5f16a6adc048c15d3d1889df]",
+	},
+	create: {
+		path: "categories[5f16a5e17c284c5c6d9a6f95].tasks[]",
+	},
+	update: {
+		get path() {
+			return this.default.path;
+		},
+		alternative: "categories[].project[5f16a6adc048c15d3d1889df].color",
+	},
+};
+
+const dicFailResponses = {
+	getPathIds: {
+		pathMissing: {
+			message: "You must include the target path:",
+			exemples: dicPathExemples.default,
+		},
+		invalidChar: {
+			message: "path: Can only contain letters, numbers, dots, and square bracket.",
+			exemples: dicPathExemples.default,
+		},
+		invalidIds: {
+			message: "Ids must be 24 char long",
+			exemples: dicPathExemples.default,
+		},
+	},
+	validatePathEnding: {
+		create: {
+			message: "path: Must end with a direct child of an array without it's id",
+			exemples: dicPathExemples.create,
+		},
+		delete: {
+			message: "path: Must end with a direct child of an array with it's id",
+			exemples: dicPathExemples.default,
+		},
+	},
+	validatePathPrecision: {
+		create: {
+			message: "path: Must include the id of the first parent inside an array",
+			exemples: dicPathExemples.create,
+		},
+		update: {
+			message: "path: Must include the id of deepest parent inside an array",
+			exemples: dicPathExemples.update,
+		},
+	},
+};
+
+function prepareQueryOptions(req, res) {
 	const arrayIds = getPathIds(req, res);
 	if (!arrayIds) return false;
-
-	const dicReqType = { POST: "create", PUT: "update", DELETE: "delete" };
-	const operationType = dicReqType[req.method];
 
 	//Match all ids including "[" and "]" but not empty ones "[]"
 	const regexPathId = /\[(?<=\[)([^\[\]]+?)(?=\])\]/g;
 	// splitPath = [ 'categories', '$[]', 'tasks', '$' ]
 	let splitPath = req.body.path.replace(regexPathId, ".$").replace(/\[\]/g, ".$[]").split(".");
 
-	// TODO Async functions Promise.all
-	switch (operationType) {
-		case "create":
-			if (!validatePathPrecision(splitPath, operationType, res)) return false;
-		case "delete":
-			if (!validatePathEnding(splitPath, operationType, res)) return false;
-			splitPath.pop();
-			break;
-		case "update":
-			if (!validatePathPrecision(splitPath, operationType, res)) return false;
-			break;
-	}
+	const operationType = { POST: "create", PUT: "update", DELETE: "delete" }[req.method];
 
+	if (!validatePathPrecision(splitPath, operationType, res)) return false;
+
+	// We remove the last "[]" for create and delete method
+	// to ensure the proper arrayFilter and childPath génération
+	if (operationType === "create" || operationType === "delete") splitPath.pop();
 	let id_index = 0;
 	let arrayFilters = [];
 	// Output arrayFilters = [{ 'i._id': 5f16a5e17c284c5c6d9a6f95 },{ 'j._id': 5f16a6adc048c15d3d1889df }]
-	// Outpu childPath = categories.$[].tasks.$[j]
+	// Output childPath = categories.$[].tasks.$[j]
 	splitPath.forEach((element, index) => {
 		if (element === "$") {
 			const setSubChar = String.fromCharCode("i".charCodeAt(0) + id_index);
@@ -125,47 +169,35 @@ function preparePath(req, res) {
 			id_index++;
 		}
 	});
-	const childPath = splitPath.join(".");
+	const childPath = splitPath.join("."); // Will be used in conjunction with arrayFilters
 
-	// In delete mode we doesn't need $setArray
-	if (operationType != "update")
-		return {
-			arrayFilters,
-			childPath,
-			target_id: arrayIds[arrayIds.length - 1],
-		};
+	delete req.body.path; // We dosn't include the path in your querry
+	req.body._id && delete req.body._id; // Let mongoose generate the id
 
-	// Prepare a precise path for $set $pull ect
-	// Prevent accidental update by removing every array inside our request body
-	// Output $setArray = { 'categories.$[i].tasks.$[j].name': 'Appeler Mathilde',}
-	delete req.body.path;
-	delete req.body._id;
+	// In delete or create mode  we doesn't need $setArray
+	if (operationType !== "update") return { arrayFilters, childPath, target_id: arrayIds[arrayIds.length - 1] };
+
+	// Prepare a precise $setArrray query option
+	// Every property inside need to be targeted precisely
+	// Otherwise all the subs entities will be deleted.
+	// Also Prevent accidental update by excluding every array potentially inside our request body
 	let $setArray = {};
-	for (key in req.body) if (!Array.isArray(req.body[key])) $setArray[`${childPath}.${key}`] = req.body[key];
-
+	for (key in req.body) {
+		if (!Array.isArray(req.body[key])) $setArray[`${childPath}.${key}`] = req.body[key];
+	}
+	// Output $setArray = { 'categories.$[i].tasks.$[j].name': 'Appeler Mathilde', etc}
 	return { $setArray, arrayFilters };
 }
 
 function getPathIds(req, res) {
 	if (!("path" in req.body)) {
-		res.status(400).json({
-			message: "you must include path:",
-			exemples: {
-				path: "categories[].tasks[5f16a6adc048c15d3d1889df]",
-				alternative: "categories[5f16a5e17c284c5c6d9a6f95].tasks[5f16a6adc048c15d3d1889df]",
-			},
-		});
+		res.status(400).json(dicFailResponses.getPathIds.pathMissing);
 		return false;
 	}
 
 	const regexValidChar = /^([a-zA-Z0-9]|[\[\]\.])+$/;
 	if (!req.body.path.match(regexValidChar)) {
-		res.status(400).json({
-			message: "path: can only contain letters, numbers, dots, and square bracket",
-			exemples: {
-				path: "categories[].tasks[5f16a6adc048c15d3d1889df]",
-			},
-		});
+		res.status(400).json(dicFailResponses.getPathIds.invalidChar);
 		return false;
 	}
 
@@ -175,8 +207,7 @@ function getPathIds(req, res) {
 	const arrayIds = req.body.path.match(regexIdExtract) || [];
 
 	if (!arrayIds.every((element) => element.length == 24)) {
-		//Status code 400 (Bad Request)
-		res.status(400).json({ message: "Bad request, ids must be a 24 char string" });
+		res.status(400).json(dicFailResponses.getPathIds.invalidIds);
 		return false;
 	}
 
@@ -184,50 +215,21 @@ function getPathIds(req, res) {
 }
 
 function validatePathEnding(splitPath, operationType, res) {
-	const dicFailResponses = {
-		create: {
-			message: "path must end with a direct child of an array without id",
-			exemple: {
-				path: "categories[5f16a5e17c284c5c6d9a6f95].tasks[]",
-			},
-		},
-		delete: {
-			message: "path must end with a direct child of an array with id",
-			exemple: {
-				path: "categories[].tasks[5f16a6adc048c15d3d1889df]",
-				alternative: "categories[5f16a5e17c284c5c6d9a6f95].tasks[5f16a6adc048c15d3d1889df]",
-			},
-		},
-	};
-	const dicLastCharMatch = { create: "$[]", delete: "$" };
-	if (splitPath[splitPath.length - 1] !== dicLastCharMatch[operationType]) {
-		res.status(400).json(dicFailResponses[operationType]);
+	// We can make atomic updates, path could end with a property (like color)
+	if (operationType === "update") return true;
+
+	if (splitPath[splitPath.length - 1] !== { create: "$[]", delete: "$" }[operationType]) {
+		res.status(400).json(dicFailResponses.validatePathEnding[operationType]);
 		return false;
 	}
 	return true;
 }
 
 function validatePathPrecision(splitPath, operationType, res) {
-	const dicFailResponses = {
-		create: {
-			message: "path: must include the id of the first parent inside an array",
-			exemple: {
-				path: "categories[5f16a5e17c284c5c6d9a6f95].tasks[]",
-			},
-		},
-		update: {
-			message: "path: must include the last id",
-			exemple: {
-				path: "categories[].tasks[5f16a5e17c284c5c6d9a6f95]",
-			},
-		},
-	};
+	if (!validatePathEnding(splitPath, operationType, res)) return false;
+	if (operationType == "delete") return true;
 
-	const dicStartIndex = {
-		create: splitPath.length - 2,
-		update: splitPath.length - 1,
-	};
-
+	const dicStartIndex = { create: splitPath.length - 2, update: splitPath.length - 1 };
 	// Look behind to see if parent array got an id
 	let isValidPath = true;
 	for (let index = dicStartIndex[operationType]; index > 0; index--) {
@@ -236,6 +238,6 @@ function validatePathPrecision(splitPath, operationType, res) {
 			break;
 		} else if (splitPath[index] == "$") break;
 	}
-	if (!isValidPath) res.status(400).json(dicFailResponses[operationType]);
+	if (!isValidPath) res.status(400).json(dicFailResponses.validatePathPrecision[operationType]);
 	return isValidPath;
 }
