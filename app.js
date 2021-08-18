26a4c33c140ba4f61fc93fec098f91e4223edf87
Lukylix <lucas.garcia.bobi@gmail.com>
2021-08-18 23:05:56 +0200

    :heavy_plus_sign: Cors for localhost

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
 require("dotenv").config();
 require("./configs/mongoose");
 
+app.use(cors({ origin: /http:\/\/localhost:.*$/ }));
+
 //BodyParser included inside express by default
 app.use(express.json());
 app.use((err, req, res, next) => {
-	if (err) return res.status(400).send({ message: "Json parsing error" });
-	next();
+  if (err) return res.status(400).send({ message: "Json parsing error" });
+  next();
 });
 
 app.get("/", (req, res) => res.status(302).json("Welcome to our api"));
@@ -22,11 +25,11 @@ app.use((req, res) => res.status(404).json({ message: "Page not found" }));
 
 // Catching errors
 app.use((err, req, res, next) => {
-	// err.status never used
-	res.status(err.status || 500).json({
-		message: err.message || "Internal Server Error",
-		err,
-	});
+  // err.status never used
+  res.status(err.status || 500).json({
+    message: err.message || "Internal Server Error",
+    err,
+  });
 });
 
 module.exports = app;
